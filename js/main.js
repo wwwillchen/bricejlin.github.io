6f9ab96fda934a13283a9b0d544a147b4a3c4bb6
Brice Lin <bricejlin@gmail.com>
2014-04-16 17:42:07 -0400

    make header sticky / shrinky

--- js/main.js
+++ js/main.js
@@ -3,3 +3,35 @@
 $(document).ready(function () {
     $('.menu').dropit();
 });
+
+$(document).ready(function () {
+    
+
+    $(window).scroll(function () {
+        var stickyNavTop = $('#main').offset().top;
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop > 10) {
+            $('#main-header').addClass('sticky');
+        } else {
+            $('#main-header').removeClass('sticky');
+        }
+    });
+    
+});
+
+  $(document).ready(function () {
+    $('#myGallery').rtg({
+      //imageWidth: auto,
+      spacing: 7,
+      categories: true,
+      categoryOptions: {
+        defaultCategory: false,
+        enableHashLinking: false,
+        includeAll: true
+      },
+      lightbox: true,
+      center: true,
+      //imageWidth: 250,
+    });
+  });
