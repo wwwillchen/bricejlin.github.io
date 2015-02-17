2527c7abf82ea112bb8d45a99561fcb7e23ae395
Brice Lin <bricejlin@gmail.com>
2015-02-17 02:42:48 -0500

    fix(reftagger): call tag function in timeout

--- hc/js/main.js
+++ hc/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
       e.preventDefault();
       $(this).trigger('hover');
     });
+    refTagger.tag(document.body);
   }, 1000);
 });
 
@@ -14,6 +15,12 @@ var refTagger = {
     socialSharing: ["twitter","facebook"]
   }
 };
+
+(function(d, t) {
+  var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+  g.src = "https://api.reftagger.com/v2/RefTagger.js";
+  s.parentNode.insertBefore(g, s);
+}(document, "script"));
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sWorker.js')
